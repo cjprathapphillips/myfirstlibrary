a90c3b7749f1281291e632025a661113ff11976b
Prathap Phillips <Prathap Phillips@DESKTOP-PRATHAP.mshome.net>
2021-04-25 22:13:34 +0530

    My First Library

---
+++ CreateMyLibrary/src/main/java/edu/java/test/PrathapLibrary.java
@@ -0,0 +1,12 @@
+package edu.java.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrathapLibrary 
+{
+    public void showMyLibrary() {
+    	System.out.println("This is Prathap library");
+    }
+}
